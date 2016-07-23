2f687bfca119da5a0f7f285dc576aa8523393770
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-07-23 07:25:02 -0400

    fix some pep8

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask, request
 from .bot import Bot
-import configparser
+# import configparser
 import os
 
 app = Flask(__name__)
 app.debug = True
 
 # Read sensible data from settings.ini file
-#config = configparser.ConfigParser()
-#config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
-#TOKEN = config.get("Bot", "token")
-#HEROKU_APP = config.get("Heroku", "app_url")
+# config = configparser.ConfigParser()
+# config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
+# TOKEN = config.get("Bot", "token")
+# HEROKU_APP = config.get("Heroku", "app_url")
 
 TOKEN = os.environ['token']
 HEROKU_APP = os.environ['app_url']
@@ -19,7 +19,8 @@ HEROKU_APP = os.environ['app_url']
 bot = Bot(TOKEN)
 json_response = bot.set_webhook(HEROKU_APP + '/{0}'.format(TOKEN))
 
+
 @app.route("/{0}".format(TOKEN), methods=['POST'])
 def hook():
-    result = bot.process_hook(request.get_json())
+    bot.process_hook(request.get_json())
     return '', 200
--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,8 +1,8 @@
 from .basebot import BaseBot
 import requests
 import time
-import json
 import re
+
 
 class Bot(BaseBot):
 
@@ -12,31 +12,44 @@ class Bot(BaseBot):
         self.time_interval_between_keyword_detection = 60
 
     def check_if_user_joined(self, response):
-        # If the messsage has the 'new chat participant' key (when a user enters a group)
-        if 'new_chat_participant' in response['message']:
+        # If the messsage has the 'new chat participant'
+        # key (when a user enters a group)
+        msg = response['message']
+        if 'new_chat_participant' in msg:
             # Check if the new participant has a first name
-            if 'first_name' in response['message']['new_chat_participant']:
-                # Use the genderize API to know if the name is a male one or a female one
-                gender_response = requests.get('https://api.genderize.io/?name={0}'.format(response['message']['new_chat_participant']['first_name']))
+            if 'first_name' in msg['new_chat_participant']:
+                # Use the genderize API to know if the name is a
+                # male one or a female one
+                first_name = msg['new_chat_participant']['first_name']
+                url = 'https://api.genderize.io/?name={0}'.format(first_name)
+                gender_response = requests.get(url)
                 # Change the welcome_message in concordance
                 if gender_response.json()['gender'] == 'female':
                     welcome_message = '<b>¡Bienvenida '
                 else:
                     welcome_message = '<b>¡Bienvenido '
 
-                welcome_message += '{0}!</b>'.format(response['message']['new_chat_participant']['first_name'])
-                json_response = self.send_message(response['message']['chat']['id'], parse_mode='HTML', text=welcome_message)
+                welcome_message += '{0}!</b>'.format(first_name)
+                self.send_message(msg['chat']['id'], parse_mode='HTML',
+                                  text=welcome_message)
 
     def check_if_someone_said_keyword(self, response):
         # If the needed time has passed since the last keyword was detected
-        if time.time() > self.last_time_someone_said_keyword + self.time_interval_between_keyword_detection:
+        if (time.time() > self.last_time_someone_said_keyword +
+                self.time_interval_between_keyword_detection):
+            msg = ('Boh. Todo el mundo sabe que el mejor IDE es '
+                   '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">'
+                   'Eclipse</a>.'),
             keywords = {
-                'ide':'Boh. Todo el mundo sabe que el mejor IDE es <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Eclipse</a>.',
+                'ide': msg,
             }
             # Check if any keyword is being used in the message
-            for word in re.sub('[!@#$?]', '', response['message']['text'].lower()).split():
+            for word in re.sub('[!@#$?]', '',
+                               response['message']['text'].lower()).split():
                 if word in keywords:
-                    json_response = self.send_message(response['message']['chat']['id'], parse_mode='HTML', text=keywords[word], disable_web_page_preview=True)
+                    self.send_message(response['message']['chat']['id'],
+                                      parse_mode='HTML', text=keywords[word],
+                                      disable_web_page_preview=True)
                     self.last_time_someone_said_keyword = time.time()
                     return True
         return False
