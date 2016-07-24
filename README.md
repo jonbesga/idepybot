1b4f345f9afb3d1852d7ce418bfdfe7535cec25f
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-25 00:18:39 +0200

    replaced settings.ini for heroku config vars

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Telegram bot skeleton ready for Heroku deployment
 
-Change the name of settings_sample.ini in the app folder to settings.ini
-
-Replace YOUR_TOKEN_HERE with your bot token.
-
 Add the heroku remote
 
 `heroku git:remote -a <YOUR_HEROKU_APP>`
@@ -11,3 +7,7 @@ Add the heroku remote
 Push the skeleton to the heroku dyno
 
 `git push heroku master`
+
+In your Heroku app control panel add the following config vars:
+
+'token' and 'app_url'
--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 app.debug = True
 
 # Read sensible data from settings.ini file
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
+#config = configparser.ConfigParser()
+#config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
+#TOKEN = config.get("Bot", "token")
+#HEROKU_APP = config.get("Heroku", "app_url")
 
-TOKEN = config.get("Bot", "token")
-HEROKU_APP = config.get("Heroku", "app_url")
+TOKEN = S3Client(os.environ['token'], os.environ['token'])
+HEROKU_APP = S3Client(os.environ['app_url'], os.environ['app_url'])
 
 # Create bot object and set webhook
 bot = Bot(TOKEN)
